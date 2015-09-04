ccfb0f55db064fd700df0b9b916a13030a59c369
mattrothenberg <mattrothenberg@gmail.com>
2015-09-04 16:11:47 -0400

    Add ng-repeat example table

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,5 +14,24 @@ angular
     'ngCookies',
     'ngResource',
     'ngSanitize',
-    'ngTouch'
-  ]);
+    'ngTouch',
+    'ui.router'
+  ])
+
+  .config(function($stateProvider, $urlRouterProvider) {
+    //
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise('/');
+    //
+    // Now set up the states
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'views/main.html'
+      })
+
+      .state('examples', {
+        url: '/examples',
+        templateUrl: 'views/examples.html' 
+      });
+  });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,22 @@
  */
 angular.module('labsDesignYeoman')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    var dataTable = [];
+    var tableLength = 100;
+    var statusArray = ['Running', 'Up', 'Down']
+
+    for (var i = 0; i < tableLength; i++) {
+      var dataObject = {
+        percent: chance.natural({min: 1, max: 100}), 
+        status: statusArray[ chance.natural({min: 0, max: statusArray.length -1}) ],
+        cpu: chance.natural({min: 1, max: 100}),
+        memory: chance.floating({min: 1, max: 1024, fixed: 2}),
+        disk: chance.floating({min: 1, max: 1024, fixed: 2}),
+        uptime: chance.natural({min: 1, max: 60})
+      };
+      dataTable.push(dataObject);
+    }  
+
+    $scope.dataTable = dataTable;
+
   });
